c68d51a1ae719c2ceeb1be4fd782fd77f20416b0
minhduc1706 <minhduc0879395@gmail.com>
2024-07-13 16:41:06 +0700

    add auth and first api

--- Food-Frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ Food-Frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,12 @@
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+import { useCreateMyUser } from "src/api/MyUserApi";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLLBACK_URL;
@@ -13,9 +15,11 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("Unable to initialze auth");
   }
 
-  const onRedirectCallback = (appState?: AppState, user?: User) =>{
-    console.log("User",user);
-  }
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+  };
 
   return (
     <Auth0Provider
