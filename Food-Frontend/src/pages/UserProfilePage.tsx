3a0b61e18f94810a781adb8c96991d19f5a6cd15
minhduc1706 <minhduc0879395@gmail.com>
2024-07-20 04:19:42 +0700

    update user

--- Food-Frontend/src/pages/UserProfilePage.tsx
+++ Food-Frontend/src/pages/UserProfilePage.tsx
@@ -1,8 +1,10 @@
+import { useUpdateMyUser } from 'src/api/MyUserApi'
 import UserProfileForm from 'src/forms/user-profile-form/UserProfileForm'
 
 const UserProfilePage = () => {
+  const {updateUser, isLoading} = useUpdateMyUser()
   return (
-    <UserProfileForm/>
+    <UserProfileForm onSave={updateUser} isLoading={isLoading}/>
   )
 }
 
