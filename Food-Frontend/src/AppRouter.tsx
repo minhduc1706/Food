a1d0444f09980588189c8b38f1dff71587df00b5
minhduc1706 <135154288+minhduc1706@users.noreply.github.com>
2024-10-23 01:17:36 +0700

    Update AppRouter.tsx

--- Food-Frontend/src/AppRouter.tsx
+++ Food-Frontend/src/AppRouter.tsx
@@ -2,7 +2,6 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import HomePage from "./pages/HomePage";
-import { lazy, Suspense } from "react";
 import LoadingSpinner from "./components/LoadingSpinner";
 import AuthCallbackPage from "./pages/AuthCallbackPage";
 import UserProfilePage from "./pages/UserProfilePage";
